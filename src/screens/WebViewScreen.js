fca36c2ebe1c277313e04254edb7f19ccc4a5fbc
Jason Hoang <jvhoang@andrew.cmu.edu>
2026-05-07 22:25:51 -0700

    changes to stop force full screening on scroll

--- src/screens/WebViewScreen.js
+++ src/screens/WebViewScreen.js
@@ -180,6 +180,8 @@ export default function WebViewScreen({ navigation }) {
         sharedCookiesEnabled
         thirdPartyCookiesEnabled
         allowsBackForwardNavigationGestures
+        allowsInlineMediaPlayback={true}
+        allowsFullscreenVideo={false}
         mediaPlaybackRequiresUserAction={false}
         userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
       />
